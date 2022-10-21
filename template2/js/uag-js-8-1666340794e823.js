8f959fad87ae28c95803fa50221d3a66c3d79a94
GiacomoDandria <73328482+GiacomoDandria@users.noreply.github.com>
2022-10-21 21:02:57 +0200

    deleted template2

--- template2/js/uag-js-8-1666340794e823.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'DOMContentLoaded', function() {
-	UAGBLottie._run( {"block_id":"1dd0a731","jsonLottie":"","lottieURl":"https:\/\/assets10.lottiefiles.com\/packages\/lf20_v7gj8hb1.json","lottieSource":"url","height":"","heightTablet":"","heightMob":"","width":185,"widthTablet":"","widthMob":"","backgroundColor":"","backgroundHColor":"","loop":true,"speed":1,"reverse":false,"playOn":"none"}, 'uagb-block-1dd0a731' );
-});
- })
